e592c1370b88d0f6fedce3203bb56cf0d747cd70
Seonil Kwon <hardid@naver.com>
2021-12-29 22:18:08 +0900

    opencv5

---
+++ opencv5.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+src = cv2.imread(r"D:\ToyProject\opencv\Image\ara.jpg", cv2.IMREAD_COLOR)
+
+print(src.shape)
+
+height, width, channel = src.shape
+matrix = cv2.getRotationMatrix2D((width/2, height/2), 90, 1)
+dst = cv2.warpAffine(src, matrix, (width, height))
+
+cv2.imshow("src", src)
+cv2.imshow("dst", dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
